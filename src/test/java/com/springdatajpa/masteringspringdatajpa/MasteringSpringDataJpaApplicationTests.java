c8eff5005d5eb673b5f1e308b6b951cf894fb1c7
Zubair624 <zubairmazumder624@gmail.com>
2025-05-08 23:35:01 +0600

    WIP: saving changes before sync

---
+++ src/test/java/com/springdatajpa/masteringspringdatajpa/MasteringSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdatajpa.masteringspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
